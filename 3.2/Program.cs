44cf303bcc8fa7ca0121123e0d901c7a9f2b3d1a
Максим Реснянский <mresnyansky@gmail.com>
2021-11-23 22:28:48 +0200

    Add project files.

---
+++ 3.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 9; i > -10 && i < 10; i += 9)
+                sum += i;
+            Console.Write(sum);
+            
+
+        }
+    }
+}
